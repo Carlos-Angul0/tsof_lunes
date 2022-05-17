4377e550ae346b42238007a0b5a5f914a28cce5e
Victor Hernandez <victortoy29@gmail.com>
2022-05-16 20:17:32 -0500

    Generalización insertar

--- controllers/Especies.php
+++ controllers/Especies.php
@@ -1,7 +1,37 @@
 <?php
+require '../libs/Database.php';
 
 class Especies{
-	public function ingresar($parametros){
-		echo "este es ingresar de Especies";
+	public function consultar($parametros){
+		$query = "SELECT * FROM especies";
+		$db = new Database();
+		return $db->ejecutarConsulta($query);
+	}
+
+	public function insertar($parametros){
+		$query = "INSERT INTO especies (nombre) 
+				VALUES ('".$parametros['nombre']."')";
+		$db = new Database();
+		return $db->ejecutarConsulta($query);
+	}
+
+	public function actualizar($parametros){
+		$query = "	UPDATE 
+						especies 
+        			SET 
+        				nombre = '$parametros[nombre]'
+        			WHERE 
+        				id = $parametros[id]";
+        $db = new Database();
+		return $db->ejecutarConsulta($query);
+	}
+
+	public function eliminar($parametros){
+		$query = "	DELETE FROM 
+						especies 
+					WHERE 
+						id = $parametros[id]";
+		$db = new Database();
+		return $db->ejecutarConsulta($query);
 	}
 }
--- controllers/Personas.php
+++ controllers/Personas.php
@@ -9,8 +9,39 @@ class Personas{
 	}
 
 	public function insertar($parametros){
-		$query = "INSERT INTO personas (nombre,altura,cedula,fecha_nacimiento) 
-				VALUES ('".$parametros['nombre']."','".$parametros['altura']."','".$parametros['cedula']."','".$parametros['fn']."')";
+		$query_new = '';
+		$query = "INSERT INTO 
+						personas
+					SET ";
+		foreach ($parametros as $key => $value) {
+			$query .= "$key= '$value',"; 
+		}
+		for ($i=0; $i <strlen($query)-1; $i++) { 
+			$query_new .= $query[$i];
+		}
+		$db = new Database();
+		return $db->ejecutarConsulta($query_new);
+	}
+
+	public function actualizar($parametros){
+		$query = "	UPDATE 
+						personas 
+        			SET 
+        				nombre = '$parametros[nombre]',
+            			cedula = $parametros[cedula],
+            			altura = $parametros[altura],
+            			fecha_nacimiento = '$parametros[fn]'
+        			WHERE 
+        				id = $parametros[id]";
+        $db = new Database();
+		return $db->ejecutarConsulta($query);
+	}
+
+	public function eliminar($parametros){
+		$query = "	DELETE FROM 
+						personas 
+					WHERE 
+						id = $parametros[id]";
 		$db = new Database();
 		return $db->ejecutarConsulta($query);
 	}
